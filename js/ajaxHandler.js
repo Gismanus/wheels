7e58e08cab28837b5f5e6db30bc53544ef2142f9
Gismanus <Gismanus@gmail.com>
2025-12-12 12:33:40 +0300

    news

--- js/ajaxHandler.js
+++ js/ajaxHandler.js
@@ -1,10 +1,15 @@
-document.querySelector('a.header-basket').addEventListener('click', function(e) {
-    console.log('lol');
-    e.preventDefault();
-    
-    fetch('/ajax_handler.php')
-    .then(response => response.json())  // скобки!
-    .then(data => {
-        document.querySelector('.content-box').innerHTML = data.content;
-    })
+document.querySelectorAll('a[href^="/"]').forEach(link => {
+    link.addEventListener('click', async (e) => {
+        e.preventDefault();
+        const url = e.target.getAttribute('href');
+        
+        const response = await fetch(url);
+        const html = await response.text();
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(html, 'text/html');
+        const content = doc.querySelector('.content').innerHTML;
+        
+        document.querySelector('.content').innerHTML = content;
+        history.pushState(null, '', url); // Обновляем URL
+    });
 });
