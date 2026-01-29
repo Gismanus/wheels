a5fabe10954238ae22c95062d56dfc342dacbed6
Gismanus <Gismanus@gmail.com>
2026-01-29 18:52:54 +0300

    туцы

--- components/footer.php
+++ components/footer.php
@@ -1,28 +1,31 @@
 <footer class="footer">
-        <div class="container footer__inner">
-            <div class="footer__col">
-                <div class="footer__logo"></div>
-                <p>Телефон: +7 (915) 177-55-21</p>
-                <p>Адрес: Муниципальный округ Коммунарка, ул. Николо-Хованская, д.20</p>
-            </div>
-            <div class="footer__col">
-                <h3>Навигация</h3>
-                <a href="products/">Каталог</a>
-                <a href="about.html">О нас</a>
-                <a href="delivery.html">Доставка</a>
-            </div>
-            <div class="footer__col">
+    <div class="container footer__inner">
+        <div class="footer__col">
+
+            <p>Телефон: +7 (915) 177-55-21</p>
+            <p>Адрес: Муниципальный округ Коммунарка, ул. Николо-Хованская, д.20</p>
+        </div>
+        <div class="footer__col">
+            <h3>Навигация</h3>
+            <a href="catalog.php">Каталог</a>
+            <a href="about.php">О нас</a>
+            <a href="delivery.php">Доставка</a>
+        </div>
+        
+        <!-- <div class="footer__col">
                 <h3>Партнёрам</h3>
                 <a href="#">Сотрудничество</a>
                 <a href="#">Документы</a>
+            </div> -->
+        <div class="footer__col">
+            <div class="footer__logo"></div>
+            
+            <div class="footer__badges">
+                <div class="footer__badge bage_steplife"></div>
+                <div class="footer__badge bage_simphony"></div>
+                <div class="footer__badge bage_armed"></div>
             </div>
-            <div class="footer__col">
 
-                <div class="footer__badges">
-                    <div class="footer__badge bage_steplife"></div>
-                    <div class="footer__badge bage_simphony"></div>
-                    <div class="footer__badge bage_armed"></div>
-                </div>
-            </div>
         </div>
-    </footer>
+    </div>
+</footer>
